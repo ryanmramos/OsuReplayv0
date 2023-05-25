5a3078023c19ae0e2c7d1d83cda14c4b90eabe65
ryanmramos <ryan.ramosenhs@gmail.com>
2023-05-24 19:18:30 -0500

    cursor appears and moves on screen as well as key presses. timing not lined up; TODO: refactor/abstract

--- OsuReplayv0/MainWindow.xaml.cs
+++ OsuReplayv0/MainWindow.xaml.cs
@@ -19,6 +19,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using OsuParsers.Replays;
+using OsuParsers.Decoders;
+using OsuParsers.Replays.Objects;
+using OsuParsers.Enums.Replays;
+using System.Windows.Threading;
 
 namespace OsuReplayv0
 {
@@ -27,9 +31,91 @@ namespace OsuReplayv0
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Ellipse circle = new Ellipse
+        {
+            Width = 10,
+            Height = 10,
+            Fill = Brushes.Red,
+            Stroke = Brushes.Black,
+            StrokeThickness = 1
+        };
+        private DispatcherTimer timer = new DispatcherTimer();
+        private ReplayFrame[] replayFrames = new ReplayFrame[] { };
+        private int currentIndex = 0;
+        private DateTime startTime = new();
+
+        private float time = 0;
+
+        public float Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+            time += 1;
+            tbTime.Text = time.ToString();
+            //Debug.WriteLine($"Time in ms: {time}");
+
+            if (currentIndex >= replayFrames.Length)
+            {
+                timer.Stop();
+                return;
+            }
+
+            ReplayFrame currentFrame = replayFrames[currentIndex];
+
+            if (currentIndex == 0)
+            {
+                Canvas.SetLeft(circle, currentFrame.X);
+                Canvas.SetTop(circle, currentFrame.Y);
+                canvas.Children.Add(circle);
+                currentIndex++;
+            }
+            else
+            {
+                double duration = Math.Abs(currentFrame.Time - replayFrames[currentIndex - 1].Time); // Duration between frames
+                double elapsed = (DateTime.Now - timer.Interval - startTime).TotalMilliseconds; // Elapsed time since last frame
+
+                if (elapsed >= duration)
+                {
+                    Canvas.SetLeft(circle, currentFrame.X);
+                    Canvas.SetTop(circle, currentFrame.Y);
+                    currentIndex++;
+                    startTime = DateTime.Now;
+                }
+                else
+                {
+                    // do nothing for now
+                }
+            }
+
+            // check for combination of button presses
+            if (currentFrame.StandardKeys == StandardKeys.K1)
+            {
+                recK1.Fill = Brushes.Gray;
+            }
+            else
+            {
+                recK1.Fill = Brushes.White;
+            }
+            if (currentFrame.StandardKeys == StandardKeys.K2)
+            {
+                recK2.Fill = Brushes.Gray;
+            }
+            else
+            {
+                recK2.Fill = Brushes.White;
+            }
         }
 
         private void btnOsu_Click(object sender, RoutedEventArgs e)
@@ -43,9 +129,7 @@ namespace OsuReplayv0
                 string path = fileDialog.FileName;
                 string fileName = fileDialog.SafeFileName;
 
-                tbInfoOsu.Text = path + "\n" + fileName;
-
-                Debug.WriteLine(tbInfoOsu.Text);
+                Debug.WriteLine($"Path: {path}\nFile name: {fileName}");
 
                 string[] lines;
 
@@ -80,190 +164,36 @@ namespace OsuReplayv0
                 string path = fileDialog.FileName;
                 string fileName = fileDialog.SafeFileName;
 
-                tbInfoOsr.Text = path + "\n" + fileName;
+                Debug.WriteLine($"Path: {path}\nFile name: {fileName}");
 
-                Debug.WriteLine(tbInfoOsr.Text);
+                Replay replay = ReplayDecoder.Decode(path);
 
-                BinaryReader replayReader = new BinaryReader(new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read));
+                Debug.WriteLine(replay.PlayerName);
 
-                Debug.WriteLine("GameMode: " + replayReader.ReadByte());
-                Debug.WriteLine("GameVersion: " + replayReader.ReadInt32());
+                replayFrames = replay.ReplayFrames.ToArray(); ;
 
-                if (replayReader.ReadByte() != 0x0b)
-                {
-                    Debug.WriteLine("MD5 Hash: (null)");
-                }
-                else
-                {
-                    int result = 0;
-                    int shift = 0;
-                    while (true)
-                    {
-                        byte b = replayReader.ReadByte();
+                //foreach (ReplayFrame rf in replay.ReplayFrames)
+                //{
+                //    float second = rf.Time / 1000.0f;
+                //    float xPos = rf.X;
+                //    float yPos = rf.Y;
+                //    StandardKeys keys = rf.StandardKeys;
 
-                        Debug.WriteLine(b);
-                        bool msb = b < 0b_1000_0000;
-                        b &= 0b_0111_1111;
-
-                        result |= b << shift;
-
-                        if (msb)
-                        {
-                            break;
-                        }
-                        shift += 7;
-                    }
-                    Debug.WriteLine(result);
-                    char[] chars = replayReader.ReadChars(result);
-                    string str = string.Join("", chars);
-
-                    Debug.WriteLine("MD5 Hash: " + str);
-                }
-
-                if (replayReader.ReadByte() != 0x0b)
-                {
-                    Debug.WriteLine("Player name: (null)");
-                }
-                else
-                {
-                    int result = 0;
-                    int shift = 0;
-                    while (true)
-                    {
-                        byte b = replayReader.ReadByte();
-
-                        Debug.WriteLine(b);
-                        bool msb = b < 0b_1000_0000;
-                        b &= 0b_0111_1111;
-
-                        result |= b << shift;
-
-                        if (msb)
-                        {
-                            Debug.WriteLine("msb reached");
-                            break;
-                        }
-                        shift += 7;
-                    }
-                    Debug.WriteLine(result);
-                    char[] chars = replayReader.ReadChars(result);
-                    string str = string.Join("", chars);
-
-                    Debug.WriteLine("Player name: " + str);
-                }
-
-                if (replayReader.ReadByte() != 0x0b)
-                {
-                    Debug.WriteLine("MD5 Hash (replay): (null)");
-                }
-                else
-                {
-                    int result = 0;
-                    int shift = 0;
-                    while (true)
-                    {
-                        byte b = replayReader.ReadByte();
-
-                        Debug.WriteLine(b);
-                        bool msb = b < 0b_1000_0000;
-                        b &= 0b_0111_1111;
-
-                        result |= b << shift;
-
-                        if (msb)
-                        {
-                            break;
-                        }
-                        shift += 7;
-                    }
-                    char[] chars = replayReader.ReadChars(result);
-                    string str = string.Join("", chars);
-
-                    Debug.WriteLine("MD5 Hash (replay): " + str);
-                }
-
-                Debug.WriteLine("# of 300s: " + replayReader.ReadUInt16());
-                Debug.WriteLine("# of 100s: " + replayReader.ReadUInt16());
-                Debug.WriteLine("# of 50s: " + replayReader.ReadUInt16());
-                Debug.WriteLine("# of Gekis: " + replayReader.ReadUInt16());
-                Debug.WriteLine("# of Katus: " + replayReader.ReadUInt16());
-                Debug.WriteLine("# of Misses: " + replayReader.ReadUInt16());
-                Debug.WriteLine("Total Score: " + replayReader.ReadInt32());
-                Debug.WriteLine("Highest Combo: " + replayReader.ReadUInt16());
-                Debug.WriteLine("Full Combo: " + (replayReader.ReadByte() == 0b_1 ? "Yes" : "No")); // TODO: double check ==
-                Debug.WriteLine("Mods : " + replayReader.ReadInt32());
-
-                if (replayReader.ReadByte() != 0x0b)
-                {
-                    Debug.WriteLine("Life bar: (null)");
-                }
-                else
-                {
-                    int result = 0;
-                    int shift = 0;
-                    while (true)
-                    {
-                        byte b = replayReader.ReadByte();
-
-                        Debug.WriteLine(b);
-                        bool msb = b < 0b_1000_0000;
-                        b &= 0b_0111_1111;
-
-                        result |= b << shift;
-
-                        if (msb)
-                        {
-                            break;
-                        }
-                        shift += 7;
-                    }
-                    Debug.WriteLine("result: " + result);
-                    char[] chars = replayReader.ReadChars(result);
-                    string str = string.Join("", chars);
-
-                    Debug.WriteLine("Life bar: " + str);
-                }
-
-                long timeStamp = replayReader.ReadInt64();
-                Debug.WriteLine("Time stamp: " + timeStamp);
-
-                DateTime centuryBegin = new DateTime(2001, 1, 1);
-                DateTime currentDate = DateTime.Now;
-
-                long elapsedTicks = timeStamp - centuryBegin.Ticks;
-                TimeSpan elapsedSpan = new TimeSpan(elapsedTicks);
-
-                DateTime finalDate = centuryBegin + elapsedSpan;
-
-                TimeZoneInfo localTimeZone = TimeZoneInfo.Local;
-                finalDate = TimeZoneInfo.ConvertTimeFromUtc(finalDate, localTimeZone);
-
-                string formattedDate = finalDate.ToString("g");
-
-                Debug.WriteLine("Formatted date: " + formattedDate);
-
-                int bytesToRead = replayReader.ReadInt32();
-                Debug.WriteLine("Length of compressed replay data: " + bytesToRead);
-
-                // TODO: store byte array
-                byte[] byteArr;
-                byteArr = replayReader.ReadBytes(bytesToRead);
-
-                Debug.WriteLine("Online Score ID: " + replayReader.ReadUInt64());
-                try
-                {
-                    Debug.WriteLine("Additional Mod Info: " + replayReader.ReadDouble());
-                }
-                catch (Exception ex) when (ex is IOException || ex is EndOfStreamException) 
-                {
-                    Debug.WriteLine("Additional Mod Info: (null)");
-                    //throw;
-                }
+                //    Debug.WriteLine($"At {second}s: Position = ({xPos},{yPos}) : Keys = {keys}");
+                //}
             }
             else
             {
                 // didnt pick anything
             }
+
+            time = 0;
+            Debug.WriteLine(replayFrames.Length);
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
     }
 }
