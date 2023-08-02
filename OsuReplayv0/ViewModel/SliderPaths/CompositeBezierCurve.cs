9f28d7edcdec82c28a2f21811b2df72519e2f175
ryanmramos <ryan.ramosenhs@gmail.com>
2023-08-02 16:57:53 -0500

    added constructor to CompositeBezierCurve with the parameter being a list of points

--- OsuReplayv0/ViewModel/SliderPaths/CompositeBezierCurve.cs
+++ OsuReplayv0/ViewModel/SliderPaths/CompositeBezierCurve.cs
@@ -11,6 +11,29 @@ namespace OsuReplayv0.ViewModel.SliderPaths
     {
         public List<BezierCurve> BezierCurves { get; set; } = new List<BezierCurve>();
 
+        // Constructor that formats the list of BezierCurves as expected given list of points
+        public CompositeBezierCurve(List<Vector2> points)
+        {
+            List<Vector2> knot = new List<Vector2> { points[0] };
+            Vector2 prevPoint = new Vector2(-200, -200);    // throwaway point
+            for (int i = 1; i < points.Count; i++)
+            {
+                Vector2 currPoint = points[i];
+                if (currPoint.X == prevPoint.X && currPoint.Y == prevPoint.Y)
+                {
+                    BezierCurves.Add(new BezierCurve(new List<Vector2>(knot)));
+                    knot.Clear();
+                    knot.Add(currPoint);
+                }
+                else
+                {
+                    knot.Add(currPoint);
+                }
+                prevPoint = currPoint;
+            }
+            BezierCurves.Add(new BezierCurve(new List<Vector2>(knot)));
+        }
+
         public double arcLen(int n)
         {
             double totalDist = 0;
