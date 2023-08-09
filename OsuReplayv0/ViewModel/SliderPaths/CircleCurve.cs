2b62617e2202fc60b999b8e74394f29843f08172
ryanmramos <ryan.ramosenhs@gmail.com>
2023-08-09 17:47:22 -0500

    added MMatrix class and a MMatrixTest to test it

--- OsuReplayv0/ViewModel/SliderPaths/CircleCurve.cs
+++ OsuReplayv0/ViewModel/SliderPaths/CircleCurve.cs
@@ -10,8 +10,15 @@ namespace OsuReplayv0.ViewModel.SliderPaths
     internal class CircleCurve
     {
 
+        // (x - h)^2 + (y - k)^2 = r^2
+        double h, k, r;
+
         public CircleCurve(Vector2 p1, Vector2 p2, Vector2 p3)
         {
+            // Matrix computation
+            // LINK: https://math.stackexchange.com/questions/213658/get-the-equation-of-a-circle-when-given-3-points
+
+            // CONT.: continuing back here after MMAtrix is defined with what I need to use it for
 
         }
     }
