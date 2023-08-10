3bfdac36febfeaa4fcf9ee81e5feecfeab64240f
ryanmramos <ryan.ramosenhs@gmail.com>
2023-08-09 19:05:07 -0500

    Removed MMatrix related classes, going to use bezier curve approximations instead

--- OsuReplayv0/ViewModel/SliderPaths/CircleCurve.cs
+++ OsuReplayv0/ViewModel/SliderPaths/CircleCurve.cs
@@ -10,15 +10,10 @@ namespace OsuReplayv0.ViewModel.SliderPaths
     internal class CircleCurve
     {
 
-        // (x - h)^2 + (y - k)^2 = r^2
-        double h, k, r;
+        // TODO: no longer going to use matrices and general circle equation. going to use bezier curve approximations
 
         public CircleCurve(Vector2 p1, Vector2 p2, Vector2 p3)
         {
-            // Matrix computation
-            // LINK: https://math.stackexchange.com/questions/213658/get-the-equation-of-a-circle-when-given-3-points
-
-            // CONT.: continuing back here after MMAtrix is defined with what I need to use it for
 
         }
     }
